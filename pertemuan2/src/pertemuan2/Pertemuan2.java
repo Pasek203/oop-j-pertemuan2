373cf82ae0dfb3cb807530a03ea37054fefe7ee7
pasek <semesterempat92@gmail.com>
2025-03-11 21:55:44 +0800

    Pertemuan2

---
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,24 @@
+
+package pertemuan2;
+
+/**
+ *
+ * @author made pasek
+ * tgl:11 maret 2025    
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Made Pasek";
+        String nim =  "2301010112";
+        System.out.println("Pertemuan 2 java");
+        System.out.println("~~~~~~~~~~~~~~~~~~");
+        System.out.println("nama   : "+nama);
+        System.out.println("NIM    :"+nim);
+        System.out.printf("nama Saya %S Dengan NiM %S,saat ini sedang belajar pemograman bahasa java",nama,nim);
+    }
+    
+}
